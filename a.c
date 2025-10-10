446b92f390d82871706d3169f26f9a574e41df48
Rein <nicaibudaoba@qq.com>
2025-10-10 22:14:55 +0800

    加入结合优先级,明天布置作业

--- a.c
+++ a.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int global_var = 50; // 定义并初始化全局变量
-
-void modify_global(void) 
-{
-    global_var = 200; // 修改全局变量
-}
-
-int main() 
-{
-    printf("修改前全局变量: %d\n", global_var);  // 50
-    modify_global();
-    printf("修改后全局变量: %d\n", global_var);  // 200
+int main() {
+    int a = 10, b = 20, c = 30, d = 40, e = 50, f = 60, g = 70, h = 80;
+    int arr[2][4] = {{a, b, c, d}, 
+                     {e, f, g, h}};
+    // 创建一个包含8个int指针的数组
+    int (*p1)[4] = arr;
+    
+    // p2 指向整个这个数组
+    int*(*p2)[4] = &p1;
+    
+    // 访问方式：
+    printf("第一个元素的值: %d\n", *(*p2)[0]);  // 输出10
+    printf("第二个元素的值: %d\n", *(*p2)[1]);  // 输出20
+    
+    // 也可以这样理解：
+    int* (*ptr_to_array)[4] = &arr;
+    
     return 0;
 }
